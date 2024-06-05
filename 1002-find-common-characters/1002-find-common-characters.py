addcd492740266cd0a994ffc6791dc08fc044f82
Harsh Wardhan Singh <151911600+HVS1995@users.noreply.github.com>
2024-06-05 11:46:31 +0530

    Time: 35 ms (98.94%), Space: 16.6 MB (93.72%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,29 +1,21 @@
 class Solution:
-    def countarray(slef,words,count):
-        for ch in words:
-            count[ord(ch) - ord('a')] += 1
-            
-            
-            
-    def commonChars(self, words: List[str]) -> List[str]:
+     def commonChars(self, words: List[str]) -> List[str]:
         result = []
-        n = len(words)
-        count = [0]*26
-        self.countarray(words[0],count)
-        
-        for i in range(1, n):
-            temp = [0] * 26
-            self.countarray(words[i], temp)
-            
-            
-            for j in range(26):
-                count[j] = min(count[j], temp[j])
-        
-        
-        for i in range(26):
-            if count[i] != 0:
-                result.extend([chr(i + ord('a'))] * count[i])
-        
+    
+        for char in range(ord('a'), ord('z')+1):
+            char = chr(char)
+            min_count = float('inf')
+
+            for word in words:
+                count = word.count(char)
+                min_count = min(min_count,count)
+
+                if min_count == 0:
+                    break
+            result.extend([char]*min_count)
+
         return result
+
+   
         
         
